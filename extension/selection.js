b89d8c76945d332bb9802d42a18d4c2a18854f0b
CoderElectronics <aristehney@gmail.com>
2024-05-15 22:07:31 -0400

    potentially fix the inactive worker issues

--- extension/selection.js
+++ extension/selection.js
@@ -1,22 +1,22 @@
 /* Nothing to see here */
 
-chrome.runtime.onInstalled.addListener(async () => {
-    chrome.contextMenus.create({
-        "id": "selectionContext",
-        "title": "Send to FakeAI",
-        "contexts": ["selection"]
-    });    
-
-    chrome.contextMenus.onClicked.addListener((item, tab) => {
+chrome.contextMenus.onClicked.addListener((item, tab) => {
         chrome.sidePanel.open({ windowId: tab.windowId }, () => {
             console.log("Context clicked")
             setTimeout(() => {
                 var port = chrome.runtime.connect({name: "fakeai_port"});
-            
+
                 port.postMessage({text: item.selectionText}, function(){
                     void chrome.runtime.lastError
                 });
             }, 100);
         });
+});
+
+chrome.runtime.onInstalled.addListener(async () => {
+    chrome.contextMenus.create({
+        "id": "selectionContext",
+        "title": "Send to FakeAI",
+        "contexts": ["selection"]
     });
 });
